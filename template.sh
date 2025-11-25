f8647419c73b55d31fd56ccb7f0eed1ee18a9429
IshanVaibhav <ishanvaibhav12@gmail.com>
2025-11-25 12:00:26 +0530

    Initial project setup

---
+++ template.sh
@@ -0,0 +1,15 @@
+#creating directory
+mdkdie -p src
+mkdir -p research
+
+#creating files
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "directory and files created successfully."
